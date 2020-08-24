be34221edaf54e33e4a9e49ac0c0156032e6aee8
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2020-08-24 20:13:32 -0300

    Ajuste de branch

--- src/components/Header/Menu/index.js
+++ src/components/Header/Menu/index.js
@@ -9,7 +9,7 @@ function Menu() {
             <nav>
                 <ul>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/">Produto</a></li>
+                    <li><a href="/">Produtos</a></li>
                     <li><a href="/">Serviços</a></li>
                     <li><a href="/">Contato</a></li>
                 </ul>
